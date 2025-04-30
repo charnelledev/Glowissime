2a503fb7516ba1723c42ad0abc20eab66c8cbee5
charnelledev <kengnemaiva54@gmail.com>
2025-04-30 15:52:59 +0100

    responsive en css

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ document.getElementById("booking-form").addEventListener("submit", function (e) 
     e.preventDefault();
     document.getElementById("confirmation-message").classList.remove("hidden");
   });
+  document.getElementById("menu-toggle").addEventListener("click", function () {
+    document.getElementById("navbar").classList.toggle("active");
+  });
+
   
