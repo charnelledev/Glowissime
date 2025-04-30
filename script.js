719523d450da1f4593f100f310f1f58541fc4898
charnelledev <kengnemaiva54@gmail.com>
2025-04-30 16:47:01 +0100

    ajout des temoignage

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-document.getElementById("booking-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    document.getElementById("confirmation-message").classList.remove("hidden");
-  });
-  document.getElementById("menu-toggle").addEventListener("click", function () {
+// document.getElementById("booking-form").addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     document.getElementById("confirmation-message").classList.remove("hidden");
+//   });
+//   document.getElementById("menu-toggle").addEventListener("click", function () {
+//     document.getElementById("navbar").classList.toggle("active");
+//   });
+
+  
+document.getElementById("menu-toggle").addEventListener("click", function () {
     document.getElementById("navbar").classList.toggle("active");
   });
-
   
