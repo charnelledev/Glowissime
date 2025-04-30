1ff55618923b472b74743f048022935c45a0b1de
charnelledev <kengnemaiva54@gmail.com>
2025-04-30 15:22:35 +0100

    script

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("booking-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    document.getElementById("confirmation-message").classList.remove("hidden");
+  });
+  
